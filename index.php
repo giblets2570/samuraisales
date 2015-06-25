ae3d4a17941cd0436cbb12228b4d6693236ff3ab
giblets2570 <tomkeohanemurray@gmail.com>
2015-06-25 16:34:05 +0100

    getting a log of the details

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
     $thankyou = "";
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    	echo "<pre>"; print_r($_POST) ;  echo "</pre>";	
-    	echo "<pre>"; print_r($_FILES) ;  echo "</pre>";
-    }
+    // if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    // 	echo "<pre>"; print_r($_POST) ;  echo "</pre>";	
+    // 	echo "<pre>"; print_r($_FILES) ;  echo "</pre>";
+    // }
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['CVFile']) && $_FILES['CVFile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['CVFile']['tmp_name'])) {
         // FIXME: add more validation, e.g. using ext/fileinfo
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -32,7 +32,6 @@
             $upload = $s3->upload($bucket, $_FILES['CVFile']['name'], fopen($_FILES['CVFile']['tmp_name'], 'rb'), 'public-read');
             $upload_url = $upload->get('ObjectURL');
             
-            $thankyou = $thankyou. " Yay :) ".$upload_url;
 
 			$name = str_replace("'","",$_POST['inputName']);
 	        $email = str_replace("'","",$_POST['inputEmail']);
@@ -40,12 +39,12 @@
 	        $availability = str_replace("'","",$_POST['inputAvailability']);
 	        $months = str_replace("'","",$_POST['inputMonths']);
 
-            $sql = "INSERT INTO signups (id, name, email, phone, availabilit  ,months, CV_url, timeCreated)
+            $sql = "INSERT INTO signups (id, name, email, phone, availability ,months, CV_url, timeCreated)
             VALUES (NULL, '$name', '$email','$phone','$availability','$months','$CV_url',NULL)";
 
             if ($conn->query($sql)) {
                 // echo "New record created successfully<br>";
-
+            	$thankyou = "Thanks for submitting!";
                 // The message
                 $message = "Name: '$name'"."\r\n"."Email: '$email'"."\r\n"."CV: '$CV_url'"."\r\n"."Hours: '$num_hours'"."\r\n"."availability: '$availability'"."\r\n"."Phone: '$phone'"."\r\n";
 
