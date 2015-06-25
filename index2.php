0043f4ff51d43b814bb0597fb8f1dcb0e825e937
giblets2570 <tomkeohanemurray@gmail.com>
2015-06-25 14:34:32 +0100

    changed things

--- index2.php
+++ index2.php
@@ -1,4 +1,9 @@
-
+<?php
+require('vendor/autoload.php');
+// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+$s3 = Aws\S3\S3Client::factory();
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
