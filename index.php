2650b7fd404cf7897e1df608d1afd51db3b4c73a
giblets2570 <tomkeohanemurray@gmail.com>
2015-06-27 11:42:05 +0100

    add

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require('vendor/autoload.php');
-    require("vendorvendor/sendgrid/sendgrid/lib/SendGrid.php");
+    require("vendor/sendgrid/sendgrid/lib/SendGrid.php");
     // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
     $s3 = Aws\S3\S3Client::factory();
     $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
