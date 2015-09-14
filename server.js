7a35a0ad844d78975fa9534bdf276c03c777286b
giblets2570 <tomkeohanemurray@gmail.com>
2015-09-14 11:15:29 +0100

    working node samurai sales

--- server.js
+++ server.js
@@ -8,25 +8,68 @@ var methodOverride = require('method-override');
 var mongoose       = require('mongoose');
 var AWS 		   = require('aws-sdk');
 var nodemailer     = require('nodemailer');
-var multer  	   = require('multer')
+var multer  	   = require('multer');
+var fs 			   = require("fs");
 // configuration ===========================================
 var awsURL = "https://s3-eu-west-1.amazonaws.com/salessamurai/";
 var done=false;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 /*Configure the multer.*/
 
 app.use(multer({ 
+	// dest: 'uploads/',
 rename: function (fieldname, filename) {
-    return filename+Date.now();
+	var re = / /gi;
+    return filename.replace(re, "-")+Date.now();
 },
 onFileUploadStart: function (file) {
   console.log(file.originalname + ' is starting ...')
+  // done=false;
 },
 onFileUploadComplete: function (file) {
   console.log(file.fieldname + ' uploaded to  ' + file.path)
   done=true;
 }
 }));
+// app.use(require('multer')({
+// 	dest: 'uploads/',
+// 	rename: function (fieldname, filename) {
+//     	return filename+Date.now();
+// 	},
+// 	limits: {
+// 		fieldNameSize: 999999999,
+// 		fieldSize: 999999999
+// 	},
+// 	includeEmptyFields: true,
+// 	inMemory: true,
+// 	onFileUploadStart: function(file) {
+// 		console.log('Starting ' + file.fieldname);
+// 	},
+// 	onFileUploadData: function(file, data) {
+// 		console.log('Got a chunk of data!');
+// 	},
+// 	onFileUploadComplete: function(file) {
+// 		console.log('Completed file!');
+// 	},
+// 	onParseStart: function() {
+// 		done=false;
+// 		console.log('Starting to parse request!');
+// 	},
+// 	onParseEnd: function(req, next) {
+// 		console.log('Done parsing!');
+// 		done=true;
+// 		next();
+// 	},
+// 	onError: function(e, next) {
+// 	if (e) {
+// 	  	console.log(e.stack);
+// 	}
+// 		next();
+// 	}
+// }));
 
 var Signup   = require('./app/models/signup.js');
 
@@ -73,42 +116,72 @@ app.use(express.static(__dirname + '/public'));
 // require('./app/routes')(app,express); // configure our routes
 
 app.post('/signup', function(req,res){
+	// console.log(req.files.CVFile);
 	if(done==true){
-	    console.log(req.files);
-	    console.log(req.body);
 	    var CV_url = awsURL + req.files.CVFile.name;
-	    console.log(CV_url);
-		var params = {
-			Bucket: bucket, 
-			Key: req.files.CVFile.name, 
-			Body: req.files.CVFile.path,
-			ACL:'public-read'};
-		s3.putObject(params, function(err, data) {
+	    // console.log(CV_url);
+	    // Asynchronous read
+		fs.readFile(req.files.CVFile.path, function (err, file_data) {
+		   	if (err) {
+		       return console.error(err);
+		   	}
+		   	var params = {
+				Bucket: bucket, 
+				Key: req.files.CVFile.name, 
+				Body: file_data,
+				ACL:'public-read'
+			};
+			s3.putObject(params, function(err, data) {
 
-	      	if (err)
-	        	console.log(err)
-	      	else
-	      		console.log("Successfully uploaded data to myBucket/myKey");
-	      	var signup = new Signup();
-			signup.name = req.body.name;
-			signup.email = req.body.email;
-			signup.phone = req.body.phone;
-			signup.availability = req.body.availability;
-			signup.months = req.body.months;
-			signup.CV_url = CV_url;
-			signup.save(function(err){
-				if(err)
-					return res.send(err);
-				return res.sendfile('./public/index.html'); // load our public/index.html file
-			});
-	   });
+		      	if (err)
+		        	console.log(err)
+		      	else
+		      		console.log("Successfully uploaded data to myBucket/myKey");
+		      	var signup = new Signup();
+				signup.name = req.body.inputName;
+				signup.email = req.body.inputEmail;
+				signup.phone = req.body.inputPhone;
+				signup.availability = req.body.inputAvailability;
+				signup.months = req.body.inputMonths;
+				signup.CV_url = CV_url;
+				signup.save(function(err){
+					if(err)
+						return res.send(err);
+					var text = "Signup,\n\n";
+	        		text += "Name: " + req.body.inputName + "\n";
+	        		text += "Email: " + req.body.inputEmail + "\n";
+	        		text += "Phone: " + req.body.inputPhone + "\n";
+	        		text += "Availability: " + req.body.inputAvailability + "\n";
+	        		text += "Months: " + req.body.inputMonths + "\n";
+	        		text += "CV_url: " + CV_url + "\n";
+	        		// console.log(text);
+					var mailOptions = {
+					    from: 'Yak Hub<admin@yakhub.co.uk>', // sender address
+					    to: 'tom@yakhub.co.uk, dan@yakhub.co.uk', // list of receivers
+					    subject: 'Samurai sales signup', // Subject line
+					    text: text, // plaintext body
+					    // html: '<b>Hello world ✔</b>' // html body
+					};
+
+					// send mail with defined transport object
+					transporter.sendMail(mailOptions, function(error, info){
+					    if(error){
+					        return console.log(error);
+					    }
+					    console.log('Message sent: ' + info.response);
+					    return res.render('../public/index',{'message':'Thanks for signing up!'}); // load our public/index.html file
+					});
+					// return res.render('../public/index',{'message':'Thanks for signing up!'}); // load our public/index.html file
+				});
+		   	});
+		});
 	}
 
 	// (id, name, email, phone, availability ,months, CV_url, timeCreated)
 });
 
 app.use(function(req, res) {
-    res.sendfile('./public/index.html'); // load our public/index.html file
+    res.render('../public/index',{'message':''}); // load our public/index.html file
 });
 
 
